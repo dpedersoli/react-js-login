d15314787e3f80de2f7a208a03fa1fcabf1ecafc
dpedersoli <dpmsengineer@gmail.com>
2023-02-06 15:42:25 -0300

    login form 50%

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,56 @@
 import { login } from './utils';
 import './index.css';
-import { useState } from 'react';
+import { useState } from "react";
 
 // Instruções:
 // * Você tem um formulário de login INCOMPLETO
 // * Não é permitido adicionar novos elementos HTML
-// * Não é permitido usar refs
+// * Não é permitido usar refs ('useRef') -> usar controlled inputs react, que usam value e onChange
 //
 // Tarefas:
-// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
-// todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
+// todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos. ~~~~DONE~~~~
+// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários. (//função espera 1 param com 2 propriedades) ~~~~DONE~~~~
 // todo - Desabilite o botão de Login equanto você está executando o login.
 // todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
-// todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
+// todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição. 
 
 export default function LoginForm() {
+  const [data, setData] = useState({email: "", password: ""});
+  const [error, setError] = useState("");
+
   return (
     <div className='wrapper'>
       <div className='login-form'>
-        <h1>Login Form 🐞</h1>
+        <h1>Login Form</h1>
         {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
-        <div className='errorMessage'></div>
+        {error &&
+        <div className='errorMessage'>
+          <p className="text-red-700 text-center">{login.message}</p>
+        </div>
+        }
         <div className='row'>
           <label htmlFor={'email'}>Email</label>
-          <input id={'email'} type={'email'} autoComplete='off' />
+          <input
+          id={'email'}
+          type={'email'}
+          autoComplete='off'
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+          />
         </div>
         <div className='row'>
           <label htmlFor={'password'}>Password</label>
-          <input id={'password'} type={'password'} />
+          <input
+          id={'password'}
+          type={'password'}
+          onChange={(e) => setData({ ...data, password: e.target.value })}
+          />
         </div>
 
         <div className='button'>
-          <button>Login</button>
+          {data.password.length < 6 || data.email.length == null
+          ? <button onClick={() => login(data)} disabled={true} style={{cursor: "not-allowed", borderColor: "transparent"}}>Login</button>
+          : <button onClick={() => login(data)} disabled={false}>Login</button>
+          }
         </div>
       </div>
     </div>
--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,14 @@
+//função espera 1 param com 2 propriedades
 export function login({ email, password }) {
-  const delay = (0.7 + Math.random() * 2) * 1000;
+  const delay = (0.7 + Math.random() * 2) * 1000
 
   return new Promise((resolve, reject) => {
     setTimeout(function () {
       if (password === 'password123' && !!email) {
-        resolve();
+        resolve()
       } else {
-        reject({ message: 'e-mail or password wrong.' });
+        reject({ message: 'e-mail ou senha incorretos.' })
       }
-    }, delay);
-  });
+    }, delay)
+  })
 }
